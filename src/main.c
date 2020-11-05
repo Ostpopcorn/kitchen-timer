a3e957183937c737fac525dc00ec9a1cc6836e41
Adrian Edin <adred130@student.liu.se>
2020-11-05 22:50:07 +0100

    First breadboard test. Minor fixes and now confirmed that code for real hardware and not just i my mind!

--- src/main.c
+++ src/main.c
@@ -17,72 +17,63 @@
 
 void app_main()
 {
-    printf("Hello world!\n");
-    ESP_LOGI(TAG, "Start rot");
+    ESP_LOGI(TAG, "Program start...");
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
-    rotary_encoder_info_t rotary_encoder_info = {0};
-    // Component init and set to half steps.
 
-    rotary_encoder_init(&rotary_encoder_info, ROT_ENC_A_GPIO, ROT_ENC_B_GPIO);
-
-    ESP_ERROR_CHECK(rotary_encoder_enable_half_steps(&rotary_encoder_info, true));
+    // Create the queues for handling events
+    QueueHandle_t button_event_queue = button_create_queue();
     QueueHandle_t rotary_encoder_event_queue = rotary_encoder_create_queue();
+    
+    // Rotary encoder init and set to half steps.
+    rotary_encoder_info_t rotary_encoder_info = {0};
+    rotary_encoder_init(&rotary_encoder_info, ROT_ENC_A_GPIO, ROT_ENC_B_GPIO);
+    ESP_ERROR_CHECK(rotary_encoder_enable_half_steps(&rotary_encoder_info, true));  
     ESP_ERROR_CHECK(rotary_encoder_set_queue(&rotary_encoder_info, rotary_encoder_event_queue));
 
+
+    // Button init 
     ESP_LOGI(TAG, "Start button");
     button_info_t button_info = {
         .inverted=false,
         .long_press_time_ms=1000
     };
-
     gpio_num_t gpio_arr[3] = {GPIO_NUM_27, GPIO_NUM_26, GPIO_NUM_NC};
     ESP_ERROR_CHECK(button_init(&button_info, gpio_arr));
-    QueueHandle_t button_event_queue = button_create_queue();
     ESP_ERROR_CHECK(button_set_queue(&button_info, button_event_queue));
 
-    printf("INIT done!!\n");
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
     while (1)
     {
-        button_event_t* ev = malloc(sizeof(button_event_t));
-        xQueueReceive(button_event_queue, ev, 100 / portTICK_PERIOD_MS);
-        if (ev)
-        {
-            if ((ev->pin == GPIO_NUM_27) )
+        button_event_t btn_ev = {0};
+        rotary_encoder_event_t rot_ev = {0};
+        while (xQueueReceive(button_event_queue, &(btn_ev),1) == pdPASS){
+            if (btn_ev.pin == GPIO_NUM_27)
             {
-                if (ev->event == BUTTON_RISING_EDGE)
+                if (btn_ev.event == BUTTON_RISING_EDGE)
                     ESP_LOGI(TAG, "27 rising edge");
-                else if (ev->event == BUTTON_FALLING_EDGE)
+                else if (btn_ev.event == BUTTON_FALLING_EDGE)
                     ESP_LOGI(TAG, "27 short press falling edge");
-                else if (ev->event == BUTTON_LONG_PRESS)
+                else if (btn_ev.event == BUTTON_LONG_PRESS)
                     ESP_LOGI(TAG, "27 long press");
-                else if (ev->event == BUTTON_FALLING_EDGE_LONG)
+                else if (btn_ev.event == BUTTON_FALLING_EDGE_LONG)
                     ESP_LOGI(TAG, "27 long press falling edge");
             }
-            else if ((ev->pin == GPIO_NUM_26) )
+            else if ((btn_ev.pin == GPIO_NUM_26) )
             {
-                if (ev->event == BUTTON_RISING_EDGE)
+                if (btn_ev.event == BUTTON_RISING_EDGE)
                     ESP_LOGI(TAG, "26 rising edge");
-                else if (ev->event == BUTTON_FALLING_EDGE)
+                else if (btn_ev.event == BUTTON_FALLING_EDGE)
                     ESP_LOGI(TAG, "26 short press falling edge");
-                else if (ev->event == BUTTON_LONG_PRESS)
+                else if (btn_ev.event == BUTTON_LONG_PRESS)
                     ESP_LOGI(TAG, "26 long press");
-                else if (ev->event == BUTTON_FALLING_EDGE_LONG)
+                else if (btn_ev.event == BUTTON_FALLING_EDGE_LONG)
                     ESP_LOGI(TAG, "26 long press falling edge");
             }
         }
-        free(ev);
+        //xQueueReceive(button_event_queue, ev,0);
+        while (xQueueReceive(rotary_encoder_event_queue, &rot_ev,1) == pdPASS){
+            ESP_LOGI(TAG,"%i",rot_ev.state.position);
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 
     ESP_ERROR_CHECK(rotary_encoder_uninit(&rotary_encoder_info));
