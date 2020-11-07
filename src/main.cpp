f2300ba42d8b730389aa1f2904b4a84fd6e0c454
Adrian Edin <adred130@student.liu.se>
2020-11-07 10:38:44 +0100

    Changes to make life in cpp easier.

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,14 @@
 
 #include "rotary_encoder.h"
 #include "button.h"
+#include "LiquidCrystal.h"
 
 #define ROT_ENC_A_GPIO GPIO_NUM_32
 #define ROT_ENC_B_GPIO GPIO_NUM_33
 #define ROT_ENC_BTN_GPIO GPIO_NUM_27
 #define TAG "MAIN"
 
-void app_main()
+extern "C" void app_main()
 {
     ESP_LOGI(TAG, "Program start...");
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
@@ -25,7 +26,7 @@ void app_main()
     QueueHandle_t rotary_encoder_event_queue = rotary_encoder_create_queue();
     
     // Rotary encoder init and set to half steps.
-    rotary_encoder_info_t rotary_encoder_info = {0};
+    rotary_encoder_info_t rotary_encoder_info = GENERATE_ROTARY_ENCODER_INFO_T;
     rotary_encoder_init(&rotary_encoder_info, ROT_ENC_A_GPIO, ROT_ENC_B_GPIO);
     ESP_ERROR_CHECK(rotary_encoder_enable_half_steps(&rotary_encoder_info, true));  
     ESP_ERROR_CHECK(rotary_encoder_set_queue(&rotary_encoder_info, rotary_encoder_event_queue));
@@ -33,18 +34,15 @@ void app_main()
 
     // Button init 
     ESP_LOGI(TAG, "Start button");
-    button_info_t button_info = {
-        .inverted=false,
-        .long_press_time_ms=1000
-    };
+    button_info_t button_info = GENERATE_BUTTON_INFO_T;
     gpio_num_t gpio_arr[3] = {GPIO_NUM_27, GPIO_NUM_26, GPIO_NUM_NC};
     ESP_ERROR_CHECK(button_init(&button_info, gpio_arr));
     ESP_ERROR_CHECK(button_set_queue(&button_info, button_event_queue));
 
     while (1)
     {
-        button_event_t btn_ev = {0};
-        rotary_encoder_event_t rot_ev = {0};
+        button_event_t btn_ev = GENERATE_BUTTON_EVENT_T;
+        rotary_encoder_event_t rot_ev = GENERAT_ROTARY_ENCODER_EVENT_T;
         while (xQueueReceive(button_event_queue, &(btn_ev),1) == pdPASS){
             if (btn_ev.pin == GPIO_NUM_27)
             {
