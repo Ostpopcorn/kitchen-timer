fc1161ac97aa34d966b89cf39e750b453cce3509
Adrian Edin <adred130@student.liu.se>
2020-11-11 20:30:55 +0100

    Added LiquidCrystal test code into main project. Rotary encoder and button are now displayed on the screen.

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,25 @@ extern "C" void app_main()
     ESP_ERROR_CHECK(button_init(&button_info, gpio_arr));
     ESP_ERROR_CHECK(button_set_queue(&button_info, button_event_queue));
 
+
+    const int numRows = 2;
+    const int numCols = 16;
+    // LCD init 
+    gpio_num_t lcd_rs = GPIO_NUM_23;
+    gpio_num_t lcd_rw = GPIO_NUM_22;
+    gpio_num_t lcd_en = GPIO_NUM_21;
+    gpio_num_t lcd_d4 = GPIO_NUM_19;
+    gpio_num_t lcd_d5 = GPIO_NUM_18;
+    gpio_num_t lcd_d6 = GPIO_NUM_5;
+    gpio_num_t lcd_d7 = GPIO_NUM_17;
+
+    
+    LiquidCrystal lcd(lcd_rs,lcd_rw,lcd_en,lcd_d4,lcd_d5,lcd_d6,lcd_d7);  
+    lcd.begin(numCols, numRows);
+    lcd.setCursor(0, 1);
+    lcd.write('1');
+    lcd.setCursor(3, 1);
+    lcd.write('1');
     while (1)
     {
         button_event_t btn_ev = GENERATE_BUTTON_EVENT_T;
@@ -46,32 +65,75 @@ extern "C" void app_main()
         while (xQueueReceive(button_event_queue, &(btn_ev),1) == pdPASS){
             if (btn_ev.pin == GPIO_NUM_27)
             {
-                if (btn_ev.event == BUTTON_RISING_EDGE)
+                lcd.setCursor(1, 1);
+                if (btn_ev.event == BUTTON_RISING_EDGE){
                     ESP_LOGI(TAG, "27 rising edge");
-                else if (btn_ev.event == BUTTON_FALLING_EDGE)
+                    lcd.write('R');
+                }
+                else if (btn_ev.event == BUTTON_FALLING_EDGE){
                     ESP_LOGI(TAG, "27 short press falling edge");
-                else if (btn_ev.event == BUTTON_LONG_PRESS)
+                    lcd.write('F');
+                }
+                else if (btn_ev.event == BUTTON_LONG_PRESS){
                     ESP_LOGI(TAG, "27 long press");
-                else if (btn_ev.event == BUTTON_FALLING_EDGE_LONG)
+                    lcd.write('L');
+                }
+                else if (btn_ev.event == BUTTON_FALLING_EDGE_LONG){
                     ESP_LOGI(TAG, "27 long press falling edge");
+                    lcd.write('X');
+                }
             }
             else if ((btn_ev.pin == GPIO_NUM_26) )
             {
-                if (btn_ev.event == BUTTON_RISING_EDGE)
+                lcd.setCursor(4, 1);
+                if (btn_ev.event == BUTTON_RISING_EDGE){
                     ESP_LOGI(TAG, "26 rising edge");
-                else if (btn_ev.event == BUTTON_FALLING_EDGE)
+                    lcd.write('R');
+                }
+                else if (btn_ev.event == BUTTON_FALLING_EDGE){
                     ESP_LOGI(TAG, "26 short press falling edge");
-                else if (btn_ev.event == BUTTON_LONG_PRESS)
+                    lcd.write('F');
+                }
+                else if (btn_ev.event == BUTTON_LONG_PRESS){
                     ESP_LOGI(TAG, "26 long press");
-                else if (btn_ev.event == BUTTON_FALLING_EDGE_LONG)
+                    lcd.write('L');
+                }
+                else if (btn_ev.event == BUTTON_FALLING_EDGE_LONG){
                     ESP_LOGI(TAG, "26 long press falling edge");
+                    lcd.write('X');
+                }
             }
         }
         //xQueueReceive(button_event_queue, ev,0);
         while (xQueueReceive(rotary_encoder_event_queue, &rot_ev,1) == pdPASS){
             ESP_LOGI(TAG,"%i",rot_ev.state.position);
-        }
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+            int number;
+            lcd.setCursor(0, 0);
+            if (rot_ev.state.position<0){
+                lcd.write('-');
+                number = -rot_ev.state.position;
+            }else{
+                lcd.write(' ');
+                number = rot_ev.state.position;
+            }
+
+            for (size_t i = 0; i < 4; i++)
+            {   
+                
+                lcd.setCursor(5-i, 0);
+                if (number <= 0)
+                {
+                    lcd.write(' ');
+                    lcd.setCursor(3-i, 0);
+                    lcd.write(' ');
+                    break;
+                }
+                lcd.write(number % 10 +'0');
+                number /= 10;
+            }
+            
+        } 
+        
     }
 
     ESP_ERROR_CHECK(rotary_encoder_uninit(&rotary_encoder_info));
